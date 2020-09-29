8b4f59e3d36bc9b6280c9efcc8fee47e166e9fa7
ruthjomo <ruthjomo19@gmail.com>
2020-09-29 12:41:26 +0300

    added codes

--- app/main/views.py
+++ app/main/views.py
@@ -36,7 +36,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "pitch login"
+    title = "blog login"
     return render_template('auth/login.html', login_form=login_form, title=title)
 
 
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export MAIL_USERNAME='ruthjomo19@gmail.com'
 export MAIL_PASSWORD='nqadwrpknbqjntjr'
 export SECRET_KEY='2603'
-export BLOGS_AP
+export BLOGS_API=''
 
 
 python3 manage.py server
