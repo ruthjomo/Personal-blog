9d598b54e44f318f62704c20d280463aa8d2cb9c
ruthjomo <ruthjomo19@gmail.com>
2020-09-30 17:42:49 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 export MAIL_USERNAME='ruthjomo19@gmail.com'
 export MAIL_PASSWORD='nqadwrpknbqjntjr'
 export SECRET_KEY='2603'
-export QUOTES_API='http://quotes.stormconsultancy.co.uk/random.json'
+# export QUOTES_API='http://quotes.stormconsultancy.co.uk/random.json'
 
 
 python3 manage.py server
